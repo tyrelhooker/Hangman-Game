355b1fdc37fd994fc71982c315ff0d388091ab5e
Tyrel Hooker <tyrelhooker@gmail.com>
2018-03-11 21:47:30 -0400

    JS file changes. Working on click. non-working keyup.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,66 +11,104 @@ var wordBank = [
   "MARTELL"
 ];
 
-var guesses = 6;
+var guessesRemaining = 5;
 var lettersGuessed = [];
 var randomWord = "";
 var randomArray = [];
-var blanks = [];
+var blanksArray = [];
 var wins = 0;
 var losses = 0;
+var userGuess;
 
-/* 
-1. Move a random string from wordBank to randomWord.   
-var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-2. Move the randomWord to randomArray and split the randomWord letters into an index. 
-randomArray = randomWord.split("");
-
-3. Loop through the randomArray add blank to blanks array for each index. 
-for (var i = 0; i < randomArray.length); i++) { 
-
+// Function that udates the score
+function updateRemainingGuesses() {
+  document.querySelector ("#game-counter").innerHTML = "<p>" + guessesRemaining + "</p";
 }
 
-4. Reset guesses variable to 6
-var guesses = 6;
+document.getElementById("startButton").addEventListener("click", setupFunction);
 
-5. Reset lettersGuessed to empty
-lettersGuess = [];
+function setupFunction() {
+  document.getElementById("instructions").innerHTML = "<p>You have entered the Game of Thrones</p>" + "<p>You must defeat five major houses. Defeating five major houses will encourage George R.R. Martin to finish the book series before the T.V. series. The game is won, if, and only if, George R.R. Martin finishes the book series before the T.V. series.</p>";
 
-*/
+  // Move a random word from wordBank to randomWord.   
+  var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  console.log(randomWord);
 
-function print(message) {
-  var outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = message;
-}
+  // Move the randomWord to randomArray and split the randomWord letters into an index. 
+  randomArray = randomWord.split("");
+  console.log(randomArray);
+
+  // Iterate through the randomArray add blanks to blanksArray for each index. 
+  blanksArray = [];
+
+  for (var i = 0; i < randomArray.length; i++) { 
+    blanksArray[i] = " _ ";
+    document.getElementById("answer-blanks").innerHTML = "<p>" + blanksArray + "</p>";
+  }
+
+  // Reset guesses variable to 10
+  guessesRemaining = 5;
+  updateRemainingGuesses();
+
+ 
+  // Reset lettersGuessed to empty
+  lettersGuessed = [];
+  document.getElementById("guessed-letters").innerHTML = "<p> [ " + lettersGuessed + " ]</p>";
+};
 
 
 
-/*
-1. Ask user to guess a letter. 
+// Ask user to guess a letter. 
+// document.getElementById("prompt").innerHTML = "<p>Please press a letter</p>";
 
-2. Capture keystroke from user. 
 document.onkeyup = function(event) {
-  var userGuess = event.key;
-}
 
-3. Verify keystroke is a letter
+  // Capture keystroke from user & converts letter to uppercase.
+  userGuess = String.fromCharCode(event.which).toUpperCase();
+  console.log(userGuess);
 
-4. Convert letter to uppercase. 
+  /* Compare the letter to lettersGuessed array, if the letter is in the lettersGuessed array --> notify user that they already guessed this letter. If the letter is not in the lettersGuessed array --> add letter to lettersGuessed array, subtract 1 from guessesRemaining, and compare letter to the randomWord array index. */
 
-5. Compare the letter to lettersGuessed array, if the letter is in the lettersGuessed array --> notify user that they already guessed this letter. 
+  // for (var j = 0; j < lettersGuessed.length; j++) {
+  //   if (lettersGuessed[j] === userGuess) {
+  //     console.log("You have already guessed this letter.");
+  //     guessesRemaining = guessesRemaining - 1;
+  //     updateRemainingGuesses();
+  //   } else {
+  //     lettersGuessed.push(userGuess);
+  //     guessesRemaining - 1;
+  //     updateRemainingGuesses();
+  //     console.log(lettersGuessed);
+  //   }
+  // }
+  /*
+  Loop through the randomWord array, if the letter is in the randomWord array, at each index where letter is found, replace the corresponding blank in the blank array. If letter is not in the randomWord array, add letter to lettersGuessed array. 
+  */
+  for (var i = 0; i < randomArray; i++) {
+    if (randomArray[i] === userGuess) {
+      // randomWord[i] replaces corresponding index value of blanksArray?
+      blanksArray[i] === randomArray[i];
+    } else {
+      lettersGuessed.push(userGuess);
 
-6. If the letter is not in the lettersGuessed array --> add letter to lettersGuessed array, subtract 1 from the countdown, and compare letter to the randomWord array index. 
-
-7. Loop through the randomWord array, if the letter is in the randomWord array, at each index where letter is found, replace the corresponding blank in the blank array.
-
-8. If letter is not in the randomWord array, add letter to lettersGuessed array. 
+    }
+  }
+};
 
 
-*/
+// 3. Verify keystroke is a letter
 
 
-/* 
+
+ 
+
+
+
+
+  
+
+
+  /* 
 1. Check to see if the user has correctly guessed all letters in randomWord array. 
 
 2. If user correctly guessed all letters --> display message and add +1 to wins counter. Remove the word from the wordBank array and store in usedWords array. Run function from #1.
@@ -90,3 +128,7 @@ if(i != -1) {
 
 
 */
+// function print(message) {
+//   var outputDiv = document.getElementById("output");
+//   outputDiv.innerHTML = message;
+// }
